08168a3b943984c88fcb563387c7c3d690e8f8d1
Levin <xlw712@163.com>
2020-01-07 14:45:54 +0800

    add: fd

--- src/main/java/com/levin/activiti/controller/ActivitiController.java
+++ src/main/java/com/levin/activiti/controller/ActivitiController.java
@@ -2,15 +2,18 @@ package com.levin.activiti.controller;
 
 import com.levin.activiti.pojo.TaskRepresentation;
 import com.levin.activiti.service.ActivitiService;
+import org.activiti.engine.history.HistoricTaskInstance;
+import org.activiti.engine.repository.ProcessDefinition;
 import org.activiti.engine.task.Task;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * https://segmentfault.com/a/1190000020298085
@@ -26,13 +29,81 @@ public class ActivitiController {
         return service.start();
     }
 
-    @RequestMapping(value = "task", method = RequestMethod.GET)
-    public List<TaskRepresentation> getTask(@RequestParam(value = "uid") String uid) {
-        List<Task> tasks = service.getTask(uid);
+    @RequestMapping(value = "startByKey", method = RequestMethod.GET)
+    public String startByKey(@RequestParam(value = "key") String key) {
+        Map<String, Object> variables = new HashMap<String, Object>();
+        variables.put("inputUser", "xiaoliwen");
+        return service.startByKey(key, variables);
+    }
+
+    @RequestMapping(value = "processList", method = RequestMethod.GET)
+    public List<ProcessDefinition> processList() {
+        return service.processlist();
+    }
+    @RequestMapping(value = "completeTaskById", method = RequestMethod.GET)
+    public void completeTaskById(@RequestParam(value = "taskId") String taskId) {
+        service.completeTaskById(taskId);
+    }
+
+    @RequestMapping(value = "todoTask", method = RequestMethod.GET)
+    public List<TaskRepresentation> getToDoTasks(@RequestParam(value = "uid") String uid, @RequestParam(value = "firstResult") int firstResult, @RequestParam(value = "maxResults")
+            int maxResults) {
+        List<Task> tasks = service.getToDoTasks(uid, firstResult, maxResults);
         List<TaskRepresentation> dtos = new ArrayList<>();
         for (Task task : tasks) {
             dtos.add(new TaskRepresentation(task.getId(), task.getName()));
         }
         return dtos;
+    }
+
+    @RequestMapping(value = "doneTask", method = RequestMethod.GET)
+    public List<TaskRepresentation> getDoneTasks(@RequestParam(value = "uid") String uid, @RequestParam(value = "firstResult") int firstResult, @RequestParam(value = "maxResults")
+            int maxResults) {
+        List<HistoricTaskInstance> tasks = service.getDoneTasks(uid, firstResult, maxResults);
+        List<TaskRepresentation> dtos = new ArrayList<>();
+        for (HistoricTaskInstance task : tasks) {
+            dtos.add(new TaskRepresentation(task.getId(), task.getName()));
+        }
+        return dtos;
+    }
+
+    @RequestMapping(value = "getToDoTotal")
+    public long getToDoTotal(@RequestParam(value = "uid") String uid) {
+        return service.getToDoTotal(uid);
+    }
+
+    @RequestMapping(value = "getDoneTotal")
+    public long getDoneTotal(@RequestParam(value = "uid") String uid) {
+        return service.getDoneTotal(uid);
+    }
+
+    /**
+     * curl -X POST -F 'file=@WechatIMG1.jpeg' http://localhost:8080/deploy
+     *
+     * @param file
+     * @return
+     */
+    @PostMapping(value = "/deploy")
+    public String deploy(@RequestParam("file") MultipartFile file) {
+        try {
+            return service.deploy(file.getOriginalFilename(), file.getInputStream());
+        } catch (IOException e) {
+            e.printStackTrace();
+            throw new RuntimeException("upload failed");
+        }
+    }
+
+    @PostMapping(value = "/deployBpmn")
+    public String deployBpmn(@RequestParam("file") MultipartFile file) {
+        try {
+            String name = file.getOriginalFilename();
+            if (!name.endsWith(".bpmn20.xm") && !name.endsWith(".bpmn")) {
+                name = name + ".bpmn";
+            }
+            return service.deploy(name, file.getInputStream());
+        } catch (IOException e) {
+            e.printStackTrace();
+            throw new RuntimeException("upload failed");
+        }
     }
 }
--- src/main/java/com/levin/activiti/service/ActivitiService.java
+++ src/main/java/com/levin/activiti/service/ActivitiService.java
@@ -1,13 +1,17 @@
 package com.levin.activiti.service;
 
-import org.activiti.engine.RuntimeService;
-import org.activiti.engine.TaskService;
+import org.activiti.engine.*;
+import org.activiti.engine.history.HistoricTaskInstance;
+import org.activiti.engine.repository.ProcessDefinition;
 import org.activiti.engine.runtime.ProcessInstance;
 import org.activiti.engine.task.Task;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.io.InputStream;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @Service
 public class ActivitiService {
@@ -15,6 +19,12 @@ public class ActivitiService {
     private RuntimeService runtimeService;
     @Autowired
     private TaskService taskService;
+    @Autowired
+    private HistoryService historyService;
+    @Autowired
+    private RepositoryService repositoryService;
+    @Autowired
+    private IdentityService identityService;
 
     /**
      * start activiti process
@@ -26,14 +36,83 @@ public class ActivitiService {
         return instance.getId();
     }
 
+    public String startByKey(String key, Map variables) {
+        return runtimeService.startProcessInstanceByKey(key, variables).getId();
+    }
+
+    public List<ProcessDefinition> processlist() {
+        List<ProcessDefinition> processDefinition = repositoryService.createProcessDefinitionQuery().orderByProcessDefinitionVersion().desc().latestVersion().list();
+        for (ProcessDefinition pd : processDefinition) {
+            System.out.println("----------------------------------------------");
+            System.out.println("流程定义名：" + pd.getName());
+            System.out.println("流程定义版本：" + pd.getVersion());
+            System.out.println("流程定义KEY：" + pd.getKey());
+            System.out.println("流程部署Deploymentid：" + pd.getDeploymentId());
+            System.out.println("流程定义id：" + pd.getId());
+        }
+        return processDefinition;
+    }
+
     /**
-     * get user task list
+     * getTodoTasks
      *
      * @param uid
-     * @return user task list
+     * @param firstResult
+     * @param maxResults
+     * @return
      */
-    public List<Task> getTask(String uid) {
-        List<Task> tasks = taskService.createTaskQuery().taskAssignee(uid).list();
-        return tasks;
+    public List<Task> getToDoTasks(String uid, int firstResult, int maxResults) {
+        return taskService.createTaskQuery().taskAssignee(uid).listPage(firstResult, maxResults);
+    }
+
+    /**
+     * getDoneTasks
+     *
+     * @param uid
+     * @param firstResult
+     * @param maxResults
+     * @return
+     */
+    public List<HistoricTaskInstance> getDoneTasks(String uid, int firstResult, int maxResults) {
+        return historyService.createHistoricTaskInstanceQuery().taskAssignee(uid).finished().listPage(firstResult, maxResults);
+    }
+
+    /**
+     * getToDoTotal
+     *
+     * @param uid
+     * @return
+     */
+    public long getToDoTotal(String uid) {
+        return taskService.createTaskQuery().taskAssignee(uid).count();
+    }
+
+    /**
+     * getDoneTotal
+     *
+     * @param uid
+     * @return
+     */
+    public long getDoneTotal(String uid) {
+        return historyService.createHistoricTaskInstanceQuery().taskAssignee(uid).count();
+    }
+
+    /**
+     * completeTaskById
+     *
+     * @param taskId
+     */
+    public void completeTaskById(String taskId) {
+        taskService.complete(taskId);
+    }
+
+    public String deploy(String name, InputStream fin) {
+        String deploymentId = repositoryService.createDeployment()
+                .addInputStream(name, fin)
+                .name(name)
+                .key(name)
+                .deploy()
+                .getId();
+        return deploymentId;
     }
 }
